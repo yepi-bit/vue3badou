f6a02c25a9e8ede050c73bd2509be690383b960e
wtj <1607562910@qq.com>
2024-02-02 09:51:55 +0800

    中间

--- src/views/rectArea.vue
+++ src/views/rectArea.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <el-input-number v-model="sideLength" :min="1" :max="100" style="margin-bottom: 12px"></el-input-number>
-    <div>1</div>
     <div class="container" :style="{ gridTemplateColumns: `repeat(${sideLength}, 1fr)` }">
       <div v-for="(area, index) in areas" :key="index" class="sub-square" :style="{ backgroundColor: area.color, gridArea: area.gridArea }"></div>
     </div>
